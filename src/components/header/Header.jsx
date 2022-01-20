c560d23e722356b0eb05305bf88388d09bf0b128
RavilGit <sefan.wen9.net@mail.ru>
2022-01-20 19:11:41 +0500

    Github pages

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,9 +9,9 @@ function Header(props) {
 
     return (
         <header className="d-flex justify-between align-center p-40">
-            <Link to='/'>
+            <Link to=''>
                 <div className="d-flex align-center">
-                    <img width={40} height={40} src="/img/logo.png" alt="logo" />
+                    <img width={40} height={40} src="img/logo.png" alt="logo" />
                     <div>
                         <h3 className="text-uppercase">React Sneakers</h3>
                         <p className="opacity-5">Магазин лучших кроссовок</p>
@@ -24,12 +24,12 @@ function Header(props) {
                     <span>{totalPrice} руб.</span>
                 </li>
                 <li className="mr-20 cu-p">
-                    <Link to="/favorites">
+                    <Link to="favorites">
                         <img width={20} height={20} src="img/heart.png" alt="Favorite" />
                     </Link>
                 </li>
                 <li>
-                    <Link to="/orders">
+                    <Link to="orders">
                         <img width={20} height={20} src="img/user.svg" alt="User" />
                     </Link>
                 </li>
